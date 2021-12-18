350b91039f9ec693b2bf6e83c7715b0ed14ee19c
YumeMichi <do4suki@gmail.com>
2021-12-18 23:07:05 +0800

    patcher: Initial ProtonAOSP bringup
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I4703271c47b03e0548aec71c48807b4ee250fa32

--- patcher.sh
+++ patcher.sh
@@ -2,6 +2,32 @@
 
 ROOT="${PWD}"
 REPOSITORIES=(
+    'bionic'
+    'bootable/recovery'
+    'build/soong'
+    'external/arm-optimized-routines'
+    'external/tinycompress'
+    'frameworks/av'
+    'frameworks/opt/net/ims'
+    'hardware/libhardware'
+    'hardware/qcom-caf/wlan'
+    'hardware/qcom/wlan'
+    'kernel/configs'
+    'packages/apps/Launcher3'
+    'packages/apps/PhoneCommon'
+    'packages/modules/common'
+    'system/libhwbinder'
+    'system/libsysprop'
+    'system/sepolicy'
+    'vendor/qcom/opensource/commonsys-intf/bluetooth'
+    'vendor/qcom/opensource/commonsys-intf/display'
+    'vendor/qcom/opensource/commonsys/packages/apps/Bluetooth'
+    'vendor/qcom/opensource/core-utils'
+    'vendor/qcom/opensource/data-ipa-cfg-mgr'
+    'vendor/qcom/opensource/dataservices'
+    'vendor/qcom/opensource/power'
+    'vendor/qcom/opensource/usb'
+    'vendor/qcom/opensource/vibrator'
 )
 
 for repository in "${REPOSITORIES[@]}"; do
